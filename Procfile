377195615d0aad06b94d2ebeaf6fc0ff8d1e7720
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-01-03 21:50:08 -0300

    repository reset

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: PYTHONPATH=$PYTHONPATH:$PWD/project gunicorn project.wsgi --log-file -
-release: python project/manage.py migrate --no-input
--- project/inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from inventory.models import Product
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['code', 'name', 'available_quantity']
-
-admin.site.register(Product, ProductAdmin)
