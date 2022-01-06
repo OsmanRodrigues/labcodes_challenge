b0f7642fd607e3e083ddef984e8417f842343a69
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-01-06 00:41:45 -0300

    category list url adjusted

--- project/inventory/urls.py
+++ project/inventory/urls.py
@@ -6,7 +6,7 @@ app_name = 'inventory'
 
 urlpatterns = [
     path('', ListInventoryEndpoint.as_view(), name='list'),
-    path('', ListCategoryEndpoint.as_view(), name='list_category'),
+    path('category', ListCategoryEndpoint.as_view(), name='list_category'),
     path('<slug:code>/update-quantity/', UpdateProductQuantityEndpoint.as_view(), name='update_product_quantity'),
     path('<slug:code>/', ProductDetailEndpoint.as_view(), name='detail'),
 ]
