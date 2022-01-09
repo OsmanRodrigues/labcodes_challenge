4ceb57a06c6638d3e513c5c5b78533becb6d9f82
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-01-09 12:48:43 -0300

    constant atm implemented

---
+++ project/assets/src/js/components/Atomic/constant.atm.js
@@ -0,0 +1,10 @@
+export const typography = {
+  font: {
+    family: '"Lato", sans-serif',
+    style: 'normal'
+  }
+};
+
+export const color = {
+  black: '#2E3942'
+}
--- project/assets/src/js/components/Atomic/index.js
+++ project/assets/src/js/components/Atomic/index.js
@@ -1,1 +1,2 @@
+export * from './constant.atm';
 export * from './Typography.atm';
