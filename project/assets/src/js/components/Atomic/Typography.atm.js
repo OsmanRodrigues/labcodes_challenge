a801788549eb00ad261394557bc5aef220e0627e
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-01-09 12:49:12 -0300

    typography styles implemented

--- project/assets/src/js/components/Atomic/Typography.atm.js
+++ project/assets/src/js/components/Atomic/Typography.atm.js
@@ -1,9 +1,38 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { color, typography } from './constant.atm';
 
-export const H1 = styled.h1``;
-export const H2 = styled.h2``;
-export const H3 = styled.h3``;
-export const Paragraph = styled.p``;
-export const DL = styled.dl``;
-export const DD = styled.dd``;
-export const DT = styled.dt``;
+const typographySharedStyle = css`
+  font-family: ${typography.font.family};
+  font-style: ${typography.font.style};
+  color: ${color.black};
+`;
+
+export const H1 = styled.h1`
+  ${typographySharedStyle}
+`;
+
+export const H2 = styled.h2`
+  ${typographySharedStyle}
+`;
+
+export const H3 = styled.h3`
+  ${typographySharedStyle}
+`;
+
+export const Paragraph = styled.p`
+  ${typographySharedStyle}
+  font-weight:  ${props => props.isBolded ? 'bold' : 'normal'};
+`;
+
+export const DL = styled.dl`
+  ${typographySharedStyle}
+`;
+
+export const DD = styled.dd`
+  ${typographySharedStyle}
+`;
+
+export const DT = styled.dt`
+  ${typographySharedStyle}
+`;
+
