aec7999aca6f5e9203756c9ac85d64721912219f
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-01-10 01:08:02 -0300

    webpack stats updated

--- project/assets/src/js/components/Atomic/index.js
+++ project/assets/src/js/components/Atomic/index.js
@@ -1,6 +1,7 @@
 export * from './Button.mol';
 export * from './Card.atm';
 export * from './constant.atm';
+export * from './GlobalStyle.atm';
 export * from './Header.atm';
 export * from './Link.atm';
 export * from './Separator.mol';
