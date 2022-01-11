f8b2e7cf5897d15dfe66aa7336cf2d8bf666f7f5
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-01-11 00:32:39 -0300

    global style adjust

--- project/assets/src/js/components/Atomic/GlobalStyle.atm.js
+++ project/assets/src/js/components/Atomic/GlobalStyle.atm.js
@@ -1,7 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
+import { color, typography, viewPort } from './constant.atm';
 
 export const GlobalStyle = createGlobalStyle`
+  html {
+     @media (max-width:${viewPort.md}) {
+      font-size: ${typography.font.size.root.md};
+    }
+  }
+
   body {
-    background-color: #E8EFF3;
+    background-color: ${color.background};
   }
 `
