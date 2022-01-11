a79aa98baaa1dee9c0d03b6aefaf3ef7655f20a5
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-01-11 00:40:52 -0300

    separator added to general layout

--- project/assets/src/js/routes.js
+++ project/assets/src/js/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductsListPage from './components/ProductsListPage';
 import ProductDetailPage from './components/ProductDetailPage';
 import { Header } from './components/Atomic/Header.atm';
+import { Separator } from './components/Atomic/Separator.mol';
 
 
 export default class Routes extends React.Component {
@@ -11,6 +12,7 @@ export default class Routes extends React.Component {
     return (
       <Router>
         <Header />
+        <Separator large/>
         <Route path='/:code/' component={ProductDetailPage} />
         <Route path='/' exact component={ProductsListPage} />
       </Router>
